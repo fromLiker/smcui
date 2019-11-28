d0647f730c573c298b3011aaa194699caa8ec408
xuexp <xuexp@cn.ibm.com>
2019-11-28 18:02:13 +0800

    add logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router'; // 路由传参用到
 import { GlobalService } from '././utils/global.service';
 
 @Component({
@@ -8,13 +9,16 @@ import { GlobalService } from '././utils/global.service';
 })
 export class AppComponent {
   constructor(
+    private router: Router,
     public globalService: GlobalService
   ) { }
   // 双向数据绑定小例子与app.component.html里的{{titile}}绑定
   title = 'Stock Market Charting Application';
 
-  onLogout() {
-    alert('onLogout!');
+  onLogout(userId: string) {
+    console.log('onLogout!', userId);
+    this.globalService.userName = '';
+    this.router.navigateByUrl('/login');
   }
 
 }
