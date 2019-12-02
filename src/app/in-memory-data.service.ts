3afaf6b5ea93c5b7d6ac6cf96b028fd8645d6aad
xuexp <xuexp@cn.ibm.com>
2019-12-02 12:54:14 +0800

    change mock data

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -3,8 +3,8 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const users = [
-      { id: 1, username: 'Root', password: '123456', usertype: 'admin', email: 'root@cn.ibm.com', mobile: '15010005159', confirmed: '1' },
-      { id: 2, username: 'Liker', password: '111111', usertype: 'user', email: 'liker@cn.ibm.com', mobile: '15010004972', confirmed: '1' }
+      { id: 1, username: 'David', password: '123456', usertype: 'admin', email: 'david@cn.ibm.com', mobile: '15042420000', confirmed: '1' },
+      { id: 2, username: 'Liker', password: '111111', usertype: 'user', email: 'liker@cn.ibm.com', mobile: '15042429999', confirmed: '1' }
     ];
     return { users };
   }
